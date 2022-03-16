71956502168f0bbd875b5a74807c0668bff08b0e
christ <christmokonda@gmail.com>
2022-03-16 16:52:23 +0100

    ADD useState in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
-import Footer from './components/Footer'
-import Graph from "./components/Graph";
-import Dropdown from "./components/Dropdown";
+import data from './data/data.json';
+import React, {useState} from "react";
+
+const parisStats = data.Paris;
+const bobigny = data.Bobigny;
+const creteil = data.Créteil;
+const nanterre = data.Nanterre;
+const versailles = data.Versailles;
+
+
 
 function App() {
+  const [city, setCity] = useState("")
+
   return (
     <div className="App">
     <h1>Indice de fragilité numérique</h1>
-      <Dropdown/>
-      <Graph/>
-      <Footer/>
+      <form action="">
+        <label>Ville:</label>
+        <input type="text"
+               value={city}
+               onChange={(e) => setCity(e.target.value)}
+        />
+      </form>
+      <p>{city}</p>
     </div>
 
 
