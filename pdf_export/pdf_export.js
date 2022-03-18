5acf248490ffd7a07035a4e70f43ce6b57b004a2
Maxime Dev <maximedaude.dev@gmail.com>
2022-03-18 18:54:03 +0100

    finish extractor function

--- pdf_export/pdf_export.js
+++ pdf_export/pdf_export.js
@@ -1,31 +1,26 @@
-//index.js
 const puppeteer = require('puppeteer')
  
-async function generatePDF() {
+async function generatePDF(city) {
 
   //We start a new browser, without showing UI
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true});
   const page = await browser.newPage();
   const url = 'http://localhost:3000';
 
   //We load the page, one of my blog post (networkidle0 means we're waiting for the network to stop making new calls for 500ms
   await page.goto(url, {waitUntil: 'networkidle0'});
-  //We add style to hide some UI elements we don't want to see on our pdf
-  await page.addStyleTag({ content:
-    `header.site-header,
-    #cookie-band,
-    .post-full-image,
-    .post-full-comments,
-    .read-next,
-    .site-footer { 
-      display: none !important;
-    }`
-  });
+
+  await page.focus("input[type=text]")
+
+  // variable Search
+  await page.keyboard.type(city)
+
+  await page.click("#root > div > div:nth-child(3) > button");
 
   //Let's generate the pdf and close the browser
-  const pdf = await page.pdf({ path: "article2.pdf", format: 'A4' });
+  const pdf = await page.pdf({ path: "article.pdf", format: 'A4' });
   await browser.close();
   return pdf;
 }
 
-generatePDF();
+// generatePDF('paris');
