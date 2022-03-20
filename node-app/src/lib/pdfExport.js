34c67bd4d28c2b7209ce75f38bee6868d4c0dde1
Maxime Dev <maximedaude.dev@gmail.com>
2022-03-20 19:49:17 +0100

    test deploy

--- node-app/src/lib/pdfExport.js
+++ node-app/src/lib/pdfExport.js
@@ -10,18 +10,18 @@ module.exports = async function generatePDF(city) {
   //We start a new browser, without showing UI
   const browser = await puppeteer.launch({ headless: true, slowMo: 50});
   const page = await browser.newPage();
-  const url = 'http://localhost:3000/';
+  const url = 'https://green-it-max.herokuapp.com/';
 
   //We load the page, one of my blog post (networkidle0 means we're waiting for the network to stop making new calls for 500ms
   await page.goto(url, {waitUntil: 'networkidle0'});
 
-  await page.focus("input[type=text]")
+  await page.focus("#root > div > div:nth-child(6) > div:nth-child(1) > input[type=text]")
 
   // variable Search
   await page.keyboard.type(city)
 
-  await page.click("#root > div > div:nth-child(3) > div:nth-child(2) > ul > li");
-  await page.click("#root > div > div:nth-child(3) > div:nth-child(1) > button");
+  await page.click("#root > div > div:nth-child(6) > div:nth-child(2) > ul > li");
+  await page.click("#root > div > div:nth-child(6) > div:nth-child(1) > button");
 
   //Let's generate the pdf and close the browser
   const pdf = await page.pdf({ path: "src/pdf_export/rapport-"+city+".pdf", format: 'A3', landscape: true });
