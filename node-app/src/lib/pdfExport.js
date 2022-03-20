3f7389dd5e3c142c6a94773d85c3ef857ad494bc
Maxime Dev <maximedaude.dev@gmail.com>
2022-03-20 17:20:54 +0100

    add function download pdf (back)/ Probleme download dans le front

--- node-app/src/lib/pdfExport.js
+++ node-app/src/lib/pdfExport.js
@@ -1,17 +1,16 @@
 const puppeteer = require('puppeteer')
- 
 
 /**
  * 
  * @param {string} city 
  * @returns Fichier pdf dans le dossier src/pdf_export
  */
-async function generatePDF(city) {
+module.exports = async function generatePDF(city) {
 
   //We start a new browser, without showing UI
-  const browser = await puppeteer.launch({ headless: true});
+  const browser = await puppeteer.launch({ headless: true, slowMo: 50});
   const page = await browser.newPage();
-  const url = 'https://green-it-max.herokuapp.com';
+  const url = 'http://localhost:3000/';
 
   //We load the page, one of my blog post (networkidle0 means we're waiting for the network to stop making new calls for 500ms
   await page.goto(url, {waitUntil: 'networkidle0'});
@@ -21,13 +20,11 @@ async function generatePDF(city) {
   // variable Search
   await page.keyboard.type(city)
 
-  await page.click("#root > div > div:nth-child(3) > button");
+  await page.click("#root > div > div:nth-child(3) > div:nth-child(2) > ul > li");
+  await page.click("#root > div > div:nth-child(3) > div:nth-child(1) > button");
 
   //Let's generate the pdf and close the browser
-  const pdf = await page.pdf({ path: "src/pdf_export/rapport-"+city+".pdf", format: 'A4' });
+  const pdf = await page.pdf({ path: "src/pdf_export/rapport-"+city+".pdf", format: 'A3', landscape: true });
   await browser.close();
-  return pdf;
+  return "src/pdf_export/rapport-"+city+".pdf";
 }
-
-//generatePDF('paris');
-// export default generatePDF
