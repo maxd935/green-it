9f676bee6e217bed33345898a5436393e2458134
gwelle <guillaume.welle.sio@gmail.com>
2022-03-20 13:41:25 +0100

    modification des srcipts K6 / resultats des tests

--- node-app/src/performance_testing_k6/soak_testing.test.js
+++ node-app/src/performance_testing_k6/soak_testing.test.js
@@ -1,17 +1,17 @@
 import http from 'k6/http';
-import {check, sleep, fail, group} from 'k6';
+import {sleep} from 'k6';
 
 export const options = {
     stages: [
         { duration: '2m', target: 400 }, // ramp up to 400 users
-        { duration: '1h30m', target: 400 }, // stay at 400 for ~4 hours
+        { duration: '1h00m', target: 400 }, // stay at 400 for ~1 hour
         { duration: '2m', target: 0 }, // scale down. (optional)
     ],
 };
 
-export default function stress_testing(){
+export default function soak_testing(){
 
-    const BASE_URL = "http://localhost:4000/"
+    const BASE_URL = "http://localhost:8800/api"
 
     let requestHeaders = {
         'User-Agent': 'k6',
@@ -19,15 +19,11 @@ export default function stress_testing(){
     };
 
     let responses = http.batch([
-        { method: 'GET', url: `${BASE_URL}6232f77b924befb4780bf4dc`, params: { headers: requestHeaders } },
-        { method: 'GET', url: `${BASE_URL}62344e6703d839472707011f`, params: { headers: requestHeaders } },
-        { method: 'GET', url: `${BASE_URL}62344e6803d8394727070123`, params: { headers: requestHeaders } },
-        { method: 'GET', url: `${BASE_URL}6232f77b924befb4780bf4dc`, params: { headers: requestHeaders } },
+        { method: 'GET', url: `${BASE_URL}33000`, params: { headers: requestHeaders } },
+        { method: 'GET', url: `${BASE_URL}77185`, params: { headers: requestHeaders } },
+        { method: 'GET', url: `${BASE_URL}94600`, params: { headers: requestHeaders } },
+        { method: 'GET', url: `${BASE_URL}75012`, params: { headers: requestHeaders } }
     ]);
-
-    /*if(!check(responses, { 'status code should be 200': (r) => r.status === 200})){
-        fail('status code was *not* 200');
-    }*/
-
+    
     sleep(1);
 }
